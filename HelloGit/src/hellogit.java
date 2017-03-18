2cf3c01bd1c83073dd99e6b73b6f3f2f9019a33c
Reenie <rutal.mahajan@gmail.com>
2017-03-18 12:09:32 +0530

    greetings comment added

--- HelloGit/src/hellogit.java
+++ HelloGit/src/hellogit.java
@@ -3,6 +3,7 @@ public class hellogit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//this is my first git project
 		System.out.println("Hello git"); 
 
 	}
